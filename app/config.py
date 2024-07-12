deb280d837106f387c9b97814f65d69a978ba8eb
leejaehun <ldyjka@gmail.com>
2024-07-12 09:13:11 +0900

    feat(master): Config 내용 추가

--- app/config.py
+++ app/config.py
@@ -10,3 +10,9 @@ def configure_app(app):
     app.config['REDIS_HOST'] = config.get('redis', 'HOST')
     app.config['REDIS_PORT'] = config.getint('redis', 'PORT')
     app.config['REDIS_DB'] = config.getint('redis', 'DB')
+    
+    app.config['WORKFLOW_ID'] = config.get('ComfyUI', 'WORKFLOW_ID')
+    app.config['API_ID'] = config.get('ComfyUI', 'API_ID')
+    app.config['WORKFLOW_FILE'] = config.get('ComfyUI', 'WORKFLOW_FILE')
+    app.config['IMAGE_DIR'] = config.get('ComfyUI', 'IMAGE_DIR')
+    
